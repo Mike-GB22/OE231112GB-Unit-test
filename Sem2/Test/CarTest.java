4a691af55ffe1261fbef720defc8c66ef09a68a0
Mipusatina <mipspam2@mail.ru>
2023-11-15 23:29:18 +0100

    Sem 2. DZ

--- Sem2/Test/CarTest.java
+++ Sem2/Test/CarTest.java
@@ -1,4 +1,44 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.assertj.core.api.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CarTest {
-  
+    public Car vehicle;
+    public int reife = 4;
+    public int driveSpeed = 60;
+    @BeforeEach
+    void setUp() {
+        vehicle = new Car("ABC", "Taz2000", 2000);
+    }
+
+    @Test
+    @DisplayName ("Car. Проверям класс")
+    void testMakeACar() {
+        assertThat(vehicle instanceof Car).isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName (value = "Car. Проверяем колеса")
+    void testReife() {
+        assertThat(vehicle.getNumWheels()).isEqualTo(reife);
+    }
+
+    @Test
+    @DisplayName ("Car. Разгоняемся")
+    void testDrive() {
+        vehicle.testDrive();
+        assertThat(vehicle.getSpeed()).isEqualTo(driveSpeed);
+    }
+
+    @Test
+    @DisplayName ("Car. Разгоняемся и паркуем")
+    void park() {
+        vehicle.testDrive();
+        vehicle.park();
+        assertThat(vehicle.getSpeed()).isEqualTo(0);
+    }
 }
--- Sem2/Test/MotorcycleTest.java
+++ Sem2/Test/MotorcycleTest.java
@@ -1,4 +1,44 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
 class MotorcycleTest {
-  
+    public Motorcycle vehicle;
+    public int reife = 2;
+    public int driveSpeed = 75;
+
+    @BeforeEach
+    void setUp() {
+        vehicle = new Motorcycle("ABC", "Drindulet1980", 1980);
+    }
+
+    @Test
+    @DisplayName ("Bike. Проверям класс")
+    void testMakeACar() {
+        assertThat(vehicle instanceof Motorcycle).isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName ("Bike. Проверяем колеса")
+    void testReife() {
+        assertThat(vehicle.getNumWheels()).isEqualTo(reife);
+    }
+
+    @Test
+    @DisplayName ("Bike. Разгоняемся")
+    void testDrive() {
+        vehicle.testDrive();
+        assertThat(vehicle.getSpeed()).isEqualTo(driveSpeed);
+    }
+
+    @Test
+    @DisplayName ("Bike. Разгоняемся и паркуем")
+    void park() {
+        vehicle.testDrive();
+        vehicle.park();
+        assertThat(vehicle.getSpeed()).isEqualTo(0);
+    }
 }
